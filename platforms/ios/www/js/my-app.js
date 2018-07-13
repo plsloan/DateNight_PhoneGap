7334bdea7884e6acca26afdf3444645c6e89ef9a
plsloan <phillipsloan24@gmail.com>
2018-07-13 16:48:41 -0400

    Left Panel Update
    The panel can be opened with swiping.
    Checkboxes look good.
    Add Date button added.

--- platforms/ios/www/js/my-app.js
+++ platforms/ios/www/js/my-app.js
@@ -1,5 +1,7 @@
 // Initialize app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    swipePanel: 'left'
+});
 
 
 // If we need to use custom DOM library, let's save it to $$ variable:
